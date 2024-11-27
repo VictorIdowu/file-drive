173a73fa4e0cd03b8b3472bfc8174778c0c6d7ee
VictorIdowu <idsonvictor@gmail.com>
2024-11-27 12:30:14 +0100

    upload dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,46 @@ import { Button } from "@/components/ui/button";
 import { useOrganization, useUser } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  title: z.string().min(1).max(200),
+  file: z
+    .custom<FileList>((val) => val instanceof FileList, "Required")
+    .refine((files) => files.length > 0, "Required"),
+});
 
 export default function Home() {
   const organization = useOrganization();
   const user = useUser();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      file: undefined,
+    },
+  });
+  const fileRef = form.register("file");
+
   let orgId: string | undefined = undefined;
   if (organization.isLoaded && user.isLoaded) {
     orgId = organization.organization?.id ?? user.user?.id;
@@ -14,18 +50,64 @@ export default function Home() {
   const createFile = useMutation(api.files.createFile);
   const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
 
-  return (
-    <main className="flex flex-col min-h-dvh items-center justify-between p-24">
-      {files?.map((file) => <div key={file._id}>{file.name}</div>)}
+  const handleSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    if (!orgId) return;
+    createFile({ name: "Text org", orgId: orgId });
+  };
 
-      <Button
-        onClick={() => {
-          if (!orgId) return;
-          createFile({ name: "Text org", orgId: orgId });
-        }}
-      >
-        Click me
-      </Button>
+  return (
+    <main className="container mx-auto mt-8">
+      <div className="flex items-center justify-between">
+        <h1 className="text-4xl font-bold">My Files</h1>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>Upload file</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="mb-6">Upload File</DialogTitle>
+              <DialogDescription>{""}</DialogDescription>
+            </DialogHeader>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(handleSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Title</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter title" {...field} />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="file"
+                  render={() => (
+                    <FormItem>
+                      <FormLabel>File</FormLabel>
+                      <FormControl>
+                        <Input type="file" {...fileRef} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
+          </DialogContent>
+        </Dialog>
+      </div>
+      {files?.map((file) => <div key={file._id}>{file.name}</div>)}
     </main>
   );
 }
