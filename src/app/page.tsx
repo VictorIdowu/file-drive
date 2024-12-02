e9a2b7e67269dbb3514da283b4b5a8a3f235ea34
VictorIdowu <idsonvictor@gmail.com>
2024-12-02 14:47:07 +0100

    empty state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { api } from "../../convex/_generated/api";
 
 import UploadButton from "@/components/UploadButton";
 import FileCard from "@/components/FileCard";
+import Image from "next/image";
 
 export default function Home() {
   const organization = useOrganization();
@@ -18,13 +19,29 @@ export default function Home() {
 
   return (
     <main className="container mx-auto my-8 px-6">
-      <div className="flex items-center justify-between">
-        <h1 className="text-4xl font-bold">My Files</h1>
-        <UploadButton />
-      </div>
-      <div className="mt-8 flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
-        {files?.map((file) => <FileCard key={file._id} file={file} />)}
-      </div>
+      {files && files?.length > 0 ? (
+        <>
+          <div className="flex items-center justify-between">
+            <h1 className="text-4xl font-bold">My Files</h1>
+            <UploadButton />
+          </div>
+          <div className="mt-8 flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
+            {files?.map((file) => <FileCard key={file._id} file={file} />)}
+          </div>
+        </>
+      ) : (
+        <div className="mt-32 flex flex-col gap-8 justify-center items-center">
+          <Image
+            src={"/empty.svg"}
+            alt=""
+            width={200}
+            height={200}
+            className=""
+          />
+          <p className="text-2xl">You have not uploaded any file yet.</p>
+          <UploadButton />
+        </div>
+      )}
     </main>
   );
 }
